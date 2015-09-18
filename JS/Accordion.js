88fffef71867fe6d8e1196726a2be2a6cb70dae9
danielgraff <ddgraff12@gmail.com>
2015-09-17 22:17:17 -0600

    end of the night

---
+++ JS/Accordion.js
@@ -0,0 +1,33 @@
+/**
+ * Created by danielgraff1 on 9/16/15.
+ */
+angular.module('myModule', ['ui.bootstrap']).controller('AccordionDemoCtrl', function ($scope) {
+    $scope.oneAtATime = true;
+
+    $scope.groups = [
+        {
+            title: 'Dynamic Group Header - 1',
+            content: 'Dynamic Group Body - 1'
+        },
+        {
+            title: 'Dynamic Group Header - 2',
+            content: 'Dynamic Group Body - 2'
+        }
+    ];
+
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
+
+    $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
+
+    $scope.removeItem = function() {
+        $scope.items.splice(-1, 1);
+    }
+
+    $scope.status = {
+        isFirstOpen: true,
+        isFirstDisabled: false
+    };
+});
